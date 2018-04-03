1d093ccfd3cbf903edc8cdb286824b3ce2f494fa
Bernardo Jr. Sein Acevedo <bernardo.sein@upr.edu>
2018-04-02 22:08:14 -0400

    ...

--- Lab6_Queue_/src/TesterSLLQueue.java
+++ Lab6_Queue_/src/TesterSLLQueue.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Tester {
+public class TesterSLLQueue {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
